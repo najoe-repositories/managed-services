c274664804d4610b4fcc9eb6eb33bb98f20387c1
harimau99 <najoe.info@gmail.com>
2022-09-11 04:44:45 +0800

    initial

---
+++ managed-cloud/managed-aws/alarm-test.py
@@ -0,0 +1,9 @@
+{
+  "Records": [
+    {
+      "Sns": {
+        "Message": "{\"AlarmName\": \"This is Test A:erts\", \"OldStateValue\": \"ALARM\", \"NewStateValue\": \"OK\", \"NewStateReason\": \"This is a rollout testing notification\"}"
+      }
+    }
+  ]
+}
